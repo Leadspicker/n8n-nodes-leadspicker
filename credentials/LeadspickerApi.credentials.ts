16179215a7f56a06b2b4bae97000166ea7da2d30
Vojtěch Oram <vojtech@leadspicker.com>
2026-03-11 10:55:16 +0100

    new version 0.4.4

--- credentials/LeadspickerApi.credentials.ts
+++ credentials/LeadspickerApi.credentials.ts
@@ -8,7 +8,7 @@ import {
 export class LeadspickerApi implements ICredentialType {
 	name = 'leadspickerApi';
 	displayName = 'Leadspicker API';
-	icon = 'file:../nodes/Leadspicker/logo_leadspicker.svg';
+	icon: ICredentialType['icon'] = 'file:../nodes/Leadspicker/logo_leadspicker.svg';
 	documentationUrl = 'https://app.leadspicker.com/app/sb/api/docs';
 	properties: INodeProperties[] = [
 		{
